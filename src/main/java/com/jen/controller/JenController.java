c5543f3218f6824a96d3df5515da9b2c19991e11
dhanuuy <dhanalakshmiy2406@gmail.com>
2024-02-20 18:19:19 +0530

    my commit

--- src/main/java/com/jen/controller/JenController.java
+++ src/main/java/com/jen/controller/JenController.java
@@ -1,5 +1,14 @@
 package com.jen.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JenController {
+	
+	@GetMapping(value = "/getMessage")
+	public String getMessage() {
+		return "Helloooo";
+	}
 
 }
